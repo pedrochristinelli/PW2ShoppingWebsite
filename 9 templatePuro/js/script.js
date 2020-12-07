8277d7d26ec2333f48f535db2e0196d1cf6bcec7
jhonatanhredondo <jhonatanhredondo@gmail.com>
2020-12-07 17:53:56 -0300

    Update script.js

--- 9 templatePuro/js/script.js
+++ 9 templatePuro/js/script.js
@@ -1,5 +1,12 @@
 var informacoesProdutos;
 
 $.ajax({
-  
-})
+  url: "https://ramos-api.herokuapp.com/produtos?pront=3007561&key=b106fee95ac6ab1a4fdbe5e480dd61ed",
+  type: "GET",
+  dataType: "html"
+}).done((response) => {
+  informacoesProdutos = JSON.parse(response);
+  console.log(informacoesProdutos);
+}).fail((jqXHR, textStatus ) => {
+    console.log("Request failed: " + textStatus);
+});
